98d9aa34a5e681431d9342db302abd283bc4b88e
Ricardo <>
2020-10-22 14:46:02 +0700

    added template to work with login and register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::get('/login', [LoginController::class, 'login']);
+Route::get('/register', [RegisterController::class , 'register']);
