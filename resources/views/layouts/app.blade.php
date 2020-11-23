7efe0a934aee9f809d6a7dae26b79214741bbb86
Ricardo <>
2020-11-23 11:59:18 +0700

    added role

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,10 +62,9 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre style="text-transform: uppercase;">
+                                    {{ Auth::user()->role }}
                                 </a>
-                                {{ Auth::user()->role }}
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
