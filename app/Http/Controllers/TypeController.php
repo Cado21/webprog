902ab74b70d98f1ef2287b3072964485b6482026
Ricardo <>
2020-11-24 12:46:08 +0700

    added some design for product detail

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Type;
+use Auth;
 class TypeController extends Controller
 {
     public function getAll() {
         $types = Type::all()->take(4);
-        return view('welcome')->with('data',$types);
+        $loggedIn = Auth::check();
+        return $loggedIn ? redirect('/product/search') : view('welcome')->with('data',$types);
     }
         
 }
