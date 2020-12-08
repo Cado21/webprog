b13d7cc50cb812b5fc32eabba76396f554ffa756
Ricardo <>
2020-12-08 21:32:53 +0700

    change seeder call

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
-        $this->call(TypeSeeder::class);
-        $this->call(ProductSeeder::class);
-        $this->call(TransactionSeeder::class);
+        $this->call([
+            UserSeeder::class,
+            TypeSeeder::class,
+            ProductSeeder::class,
+            TransactionSeeder::class,
+        ]);
     }
 }
